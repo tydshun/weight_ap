27d2e8b297d7923a2ec29fea6b5a12f6a62c1622
tydshun <69792117+tydshun@users.noreply.github.com>
2021-04-09 15:45:49 +0900

    ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
  
   root to: 'tweets#index'
-  devise_for :users
+
+  resources :tweets do
+    collection do
+      get 'tweets'
+      get 'search'
+    end
+  end
+
   # resources :blogs
   # resources :users
   # resources :weights
