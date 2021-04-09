90f5a7520e8fc4d57645958afe2c386a19c24b95
tydshun <69792117+tydshun@users.noreply.github.com>
2021-04-09 12:11:46 +0900

    バリデーションとアソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  validates :profile, length: { maximum: 200 }
+
+  has_many :blogs
+  has_many :weights
+  has_one_attached :image
+
+  has_one_attached :avater
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
