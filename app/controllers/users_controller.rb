b55eadfd66ce1871fa0517ab39eff66780c3d5e9
tydshun <69792117+tydshun@users.noreply.github.com>
2021-04-15 01:43:06 +0900

    プロフィールページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @tweets = Tweet.page(params[:page]).per(6).order('created_at DESC')
+    @tweets = current_user.tweets.page(params[:page]).per(6).order('created_at DESC')
+  end
+
+  def edit
+    @user = current_user
+    @tweets = current_user.tweets.page(params[:page]).per(6).order('created_at DESC')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, :only => [:show]
+  resources :users, :only => [:show, :edit]
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
