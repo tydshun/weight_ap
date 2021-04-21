cf9c1e141b56e124aee6d7a9d049ced08302ddd1
tydshun <69792117+tydshun@users.noreply.github.com>
2021-04-21 20:37:48 +0900

    小数点の問題を解決

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -2,9 +2,11 @@ class WeightsController < ApplicationController
   def index
     @weight = Weight.new
     # @today_weight = Weight.average(:weight_body).round(2)
-    @today_weight = Weight.where(created_at: Time.zone.now.all_day).average(:weight_body).round(2)
+    @today_weight = Weight.where(created_at: Time.zone.now.all_day).average(:weight_body)
     @week_weight = Weight.where(created_at: 1.week.ago.all_day).average(:weight_body)
     @month_weight = Weight.where(created_at: 1.month.ago.all_day).average(:weight_body)
+    # @bmi = (@today_weight / (height2 ** 2))
+    # @height1 = 
     @user = current_user
   end
   
