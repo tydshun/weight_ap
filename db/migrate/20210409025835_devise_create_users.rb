97f5d1c881928b47e4a71ab5e010bf2cb8bf464c
tydshun <69792117+tydshun@users.noreply.github.com>
2021-04-22 04:00:34 +0900

    userテーブルに身長のカラムを追加

--- db/migrate/20210409025835_devise_create_users.rb
+++ db/migrate/20210409025835_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,           null: false
       t.text   :profile
+      t.integer :height, precision: 3
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
