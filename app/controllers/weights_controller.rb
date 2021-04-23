bec7b26a7a06962de83bbfa1042ea0148c3415e4
tydshun <69792117+tydshun@users.noreply.github.com>
2021-04-24 02:45:18 +0900

    1ヶ月の体重データを扱えるよう記述

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -4,14 +4,16 @@ class WeightsController < ApplicationController
     @user_data = Weight.where(user_id: current_user.id)
     @user = current_user
     if @user_data.present?
-      @today_weight = @user_data.where(created_at: Time.zone.now.all_day).average(:weight_body).round(2)
+      @today_weight = @user_data.where(created_at: Time.zone.now.all_day).average(:weight_body)
       @week_weight = @user_data.where(created_at: 1.week.ago.all_day).average(:weight_body)
       @month_weight = @user_data.where(created_at: 1.month.ago.all_day).average(:weight_body)
-      @bmi = (@today_weight / (@user.height * @user.height) * 10000)
-      @standard_weight = (@user.height * @user.height * 22.0 / 10000)
-      @difference_weight = @today_weight - @standard_weight
-      if @week_weight && @user.height.present?
-        @bmi_week = (@week_weight / (@user.height * @user.height) * 10000.0)
+      if @today_weight.present?
+        @bmi = (@today_weight / (@user.height * @user.height) * 10000)
+        @standard_weight = (@user.height * @user.height * 22.0 / 10000)
+        @difference_weight = @today_weight - @standard_weight
+        if @week_weight && @user.height.present?
+          @bmi_week = (@today_weight / (@user.height * @user.height) * 10000.0)
+        end
       end
     end
   end
@@ -20,18 +22,25 @@ class WeightsController < ApplicationController
     @weight = Weight.new
     @user_data = Weight.where(user_id: current_user.id)
     @user = current_user
+    # ユーザーが体重を記録していなければ、今週の体重はない。
     if @user_data.present?
-      @week_weight = @user_data.where(created_at: 1.week.ago.all_day).average(:weight_body)
-      @month_weight = @user_data.where(created_at: 1.month.ago.all_day).average(:weight_body)
-      @bmi = (@today_weight / (@user.height * @user.height) * 10000)
+      @week_weight = @user_data.where(created_at: 7.days.ago.beginning_of_day..Time.zone.now.end_of_day).average(:weight_body)
+    end
+    # 下記で30日前の週の平均体重から今日まで体重のデータを取得
+    @month_weight = @user_data.where(created_at: 37.days.ago.beginning_of_day..31.days.ago.end_of_day).average(:weight_body)
+    if @month_weight && @user.height.present? 
+      @bmi = (@month_weight / (@user.height * @user.height) * 10000)
       @standard_weight = (@user.height * @user.height * 22.0 / 10000)
-      @difference_weight = @week_weight - @standard_weight
-      if @week_weight && @user.height.present?
-        @bmi_week = (@week_weight / (@user.height * @user.height) * 10000.0)
-      end
+      @difference_weight = @month_weight - @standard_weight
+      @bmi_month = (@month_weight / (@user.height * @user.height) * 10000.0)
+    end
+    # 30日前の週の平均と今週の平均の体重の比較
+    if @month_weight && @week_weight.present?
+      @wm_weight = (@week_weight - @month_weight)
     end
   end
   
+
   def create
     @user = current_user
     @weight = Weight.new(weight_params)
