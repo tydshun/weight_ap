c7c0a7e19c08831c361940104e34a917af71f460
tydshun <69792117+tydshun@users.noreply.github.com>
2021-05-01 16:13:29 +0900

    非公開設定の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
-  # validates :profile, length: { maximum: 200 }
+  validates :profile, length: { maximum: 200 }
   has_many :tweets, dependent: :destroy
   has_many :comments , dependent: :destroy
   has_many :blogs
   has_many :weights
   has_one_attached :image
+
 
   has_one_attached :avatar
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_121338) do
+ActiveRecord::Schema.define(version: 2021_04_29_073958) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_121338) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", limit: 1, default: 1, null: false
     t.index ["user_id"], name: "index_weights_on_user_id"
   end
 
