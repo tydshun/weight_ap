e138cd979ca59eb8f7f410e1bc62200d5265287e
tydshun <69792117+tydshun@users.noreply.github.com>
2021-05-01 19:01:27 +0900

    モデルの記述

---
+++ app/models/blog.rb
@@ -0,0 +1,14 @@
+class Blog < ApplicationRecord
+  validates :user_id, presence:true
+  validates :title, presence: true,length: { in: 1..20}
+  belongs_to :user
+
+  def self.search(search)
+    
+    if search
+      Blog.where('title LIKE ? OR content LIKE ?', "%#{search}%","%#{search}%")
+    else
+      Blog.all
+    end
+  end
+end
